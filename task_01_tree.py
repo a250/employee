41601cdaf8449534f6f8b2a27ebdb22287dd2522
Alexey Zyablitsev <az@azso.ru>
2020-09-16 13:19:00 +0300

    first: tree class reqcursion and loop explore

---
+++ task_01_tree.py
@@ -0,0 +1,102 @@
+import random 
+
+class Node():
+
+    def __init__(self, ind, value):
+        self.id = ind
+        self.val = value
+
+class Tree():
+    def __init__(self):
+        # генерируем дерево вида 
+        # {node_id: {'node': node_obj, 'link': [childs node_id,...,...]}}
+
+        self.tree = dict()    
+        self.root_id = 0        
+        _nd = Node(self.root_id, 'root')
+        self.tree[self.root_id] = {'node': _nd, 'links': []}
+        self.nodes = 1
+
+    def addChilds(self, parent_id, values = []):
+        child_nodes = [Node(i[0],i[1]) for i in zip(range(self.nodes, self.nodes+len(values)), values)]
+        self.nodes += len(values)
+        child_id = []
+        for nd in child_nodes:
+            self.tree[nd.id] = {'node': nd, 'links': [parent_id]}
+            child_id.append(nd.id)
+        
+        self.tree[parent_id]['links'] += [nd.id for nd in child_nodes]
+        return child_id
+    
+    def createRandomTree(self, prob, parents = [], level = 0):
+
+        if level == 0:
+            parents = [self.root_id]
+            
+        for p in parents:
+            if (random.random()<prob) or (level<=2):
+
+                qty = random.randint(1,2)
+                add_nodes = [('val_'+str(random.randint(1,100))) for i in range(qty)]
+                _parent = self.addChilds(p, add_nodes)
+  
+                self.createRandomTree(prob, _parent, level +1)
+                
+    def getTreeValue(self):
+        # возвращает дерево вида: 
+        #{node_id: {'node': node.val, 'link': [childs node_id,...,...]}}
+        
+        return {k: {'node': self.tree[k]['node'].val, 'links': self.tree[k]['links']} for k in self.tree.keys()}
+
+    
+    def getTree(self):
+        # возвращает дерево вида:
+        #{node_id: {'node': node_obj, 'link': [childs node_id,...,...]}}
+        
+        return self.tree
+      
+      
+def displayTreeRecursive(nodes = [], level = 0):
+    if level ==0:
+        nodes = tree_dict[0]['links']
+        print(tree_dict[0]['node'].val)
+   
+    for n in nodes:
+
+        print(f'-{"--"*level*2}',f"{tree_dict[n]['node'].id}: {tree_dict[n]['node'].val}")
+        exploreTree(tree_dict[n]['links'][1:], level +1)
+    
+def exploreTreeLoop():
+    tree = [0] + tree_dict[0]['links']
+    pos = 0
+    while pos < len(tree_dict.keys()):
+
+        # do something with nodes
+        print(f"Processing node: {tree[pos]}:{tree_dict[pos]['node'].val}")   
+
+        links = tree_dict[tree[pos]]['links'][1:]
+        if len(links) >0:
+            tree += links
+        pos +=1
+        
+        
+        
+t = Tree()
+t.createRandomTree(0.5)
+print(f'\ncreated tree with {len(t.getTree().keys())} nodes')
+
+import pprint
+
+tree_dict = t.getTree()
+
+print('\nShow tree with node objects: \n')
+pprint.pprint(tree_dict)
+
+print('\nShow tree with node values: \n')
+pprint.pprint(t.getTreeValue())
+
+print('\nExplore tree with recurion\n')
+displayTreeRecursive()
+
+print('\nExplore tree with loop\n')
+exploreTreeLoop()
